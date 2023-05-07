8aa1fa0c13261c9a0b56f69fa636515766c58cf5
BDLombardi <squatwaboo@live.com>
2023-05-07 16:34:03 -0700

    fixed file size

---
+++ build_champ_data.py
@@ -0,0 +1,98 @@
+import json
+import time
+from riotwatcher import LolWatcher, ApiError, RiotWatcher
+import random
+
+api_key = 'RGAPI-7c56ac6e-237c-476b-9ca9-4ee2fe089ca4'  
+watcher1 = LolWatcher(api_key)
+my_region = 'na1'
+
+
+champ_mast_labels = dict()
+champs = watcher1.data_dragon.champions("13.7.1")
+for champ in champs["data"].keys():
+    champ_mast_labels[champ.lower()] = int(champs["data"][champ]["key"])
+print(champ_mast_labels["jax"])
+outfile = open("champ_labels.json","w")
+json.dump(champ_mast_labels,outfile,indent=4)
+outfile.close()
+
+print("Opening Dataset")
+start = time.time()
+infile = open("data3.json","r")
+dataset = json.load(infile)
+infile.close()
+end = time.time()
+
+print(len(dataset))
+print(end-start)
+
+champ_mast_labels = dict()
+champs = watcher1.data_dragon.champions("13.7.1")
+for champ in champs["data"].keys():
+    champ_mast_labels[champ.lower()] = int(champs["data"][champ]["key"])
+
+print(champ_mast_labels)
+#position_dict = {"TOP":0,"JUNGLE":1,"MIDDLE":2,"BOTTOM":3, "UTILITY":4}
+team_list = []
+y = []
+for match in dataset:
+    win_team = []
+    lose_team = []
+    bad_match = False
+    if match["info"]["gameMode"]== "CLASSIC":
+        for player in match["info"]["participants"]:
+            if len(match["info"]["participants"]) == 10 and len(match["metadata"]["participants"]) == 10:
+                if player["puuid"] == "BOT" or "challenges" not in player.keys():
+                    bad_match = True
+                else:
+                    champ_id = champ_mast_labels[player["championName"].lower()]
+                    if player["win"]==True:
+                        win_team.append(champ_id)
+                        if match["info"]["participants"].index(player) == 4:
+                            try: 
+                                win_team.append(player["challenges"]["teamBaronKills"])
+                                win_team.append(player["challenges"]["teamElderDragonKills"])
+                                win_team.append(player["challenges"]["teamRiftHeraldKills"])
+                            except:
+                                print(1)
+                                outfile = open("match.json","w")
+                                json.dump(match,outfile,indent=4)
+                                outfile.close()
+                                exit()
+                    else:
+                        lose_team.append(champ_id)
+                        if match["info"]["participants"].index(player)  == 9:
+                            try:
+                                lose_team.append(player["challenges"]["teamBaronKills"])
+                                lose_team.append(player["challenges"]["teamElderDragonKills"])
+                                lose_team.append(player["challenges"]["teamRiftHeraldKills"])
+                            except:
+                                print(2)
+                                outfile = open("match.json","w")
+                                json.dump(match,outfile,indent=4)
+                                outfile.close()
+                                exit()
+            else: 
+                bad_match = True
+        if bad_match == False:
+            swap = random.randint(0,1)
+            if swap ==0:
+                win_team.extend(lose_team)
+                team_list.append(win_team)
+            else: 
+                lose_team.extend(win_team)
+                team_list.append(lose_team)
+            y.append(swap)
+    else:
+        pass
+
+
+print("Success, writing data ")
+outfile = open("champ_data4.json","w")
+json.dump(team_list,outfile,indent=4)
+outfile.close()
+outfile = open("win_data4.json","w")
+json.dump(y,outfile,indent=4)
+outfile.close()
+
---
+++ build_data_set.py
@@ -0,0 +1,78 @@
+import json 
+from riotwatcher import LolWatcher, ApiError, RiotWatcher
+import copy
+
+
+api_key = 'RGAPI-7c56ac6e-237c-476b-9ca9-4ee2fe089ca4'
+watcher1 = LolWatcher(api_key)
+my_region = 'na1'
+
+# infile = open("data.json","r")
+# dataset = json.load(infile)
+# infile.close()
+
+infile = open("annotations.json","r")
+annotations = json.load(infile)
+infile.close()
+match_dict = annotations["matches"]
+name_dict = annotations["names"]
+
+i = 0
+
+stable_names = copy.deepcopy(list(name_dict.keys()))
+stable_names = stable_names[-1000:]
+
+dataset = []
+for key in stable_names:
+    my_match_ids = watcher1.match.matchlist_by_puuid(my_region, key ,count=100)
+    data = []
+    for entry in my_match_ids: 
+        data.append(watcher1.match.by_id(my_region,entry))
+    for entry in data:
+        try:
+            if match_dict[entry["metadata"]["matchId"]] == True:
+                pass
+        except:
+            match_dict[entry["metadata"]["matchId"]] = True
+            dataset.append(entry)
+        for name in entry["metadata"]["participants"]:
+            try:
+                if name_dict[name] == True:
+                    pass
+            except:
+                name_dict[name] = True
+    print(len(match_dict))
+
+    print(len(name_dict))
+
+    annotations = dict()
+    annotations["names"] = name_dict
+    annotations["matches"] = match_dict
+    outfile = open("annotations.json","w")
+    json.dump(annotations,outfile,indent=4)
+    outfile.close()
+
+    outfile = open("data3.json","w")
+    json.dump(dataset,outfile,indent=4)
+    outfile.close()
+    i +=1 
+    print(i)
+    print(len(dataset))
+    if i == 1000: 
+        break
+
+# print(len(match_dict))
+
+# print(len(name_dict))
+
+# annotations = dict()
+# annotations["names"] = name_dict
+# annotations["matches"] = match_dict
+# outfile = open("annotations.json","w")
+# json.dump(annotations,outfile)
+# outfile.close()
+
+# outfile = open("data.json","w")
+# json.dump(dataset,outfile)
+# outfile.close()
+
