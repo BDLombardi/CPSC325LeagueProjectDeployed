7975bd528f91187bd40e2dbfd076a2355878a577
BDLombardi <squatwaboo@live.com>
2023-05-08 16:28:12 -0700

    update

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ if options == "Account Summary":
     if text_input != "":
         username = text_input
         #api_key = 'RGAPI-7c56ac6e-237c-476b-9ca9-4ee2fe089ca4'  
+        st.write(st.secrets.keys())
         api_key = st.secrets["key"]
         watcher1 = LolWatcher(api_key)
         my_region = 'na1'
